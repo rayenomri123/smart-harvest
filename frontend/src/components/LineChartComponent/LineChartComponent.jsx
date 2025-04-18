fa9eb1e213f4755eeec2ee4d355d3081c9beb574
rayenomr123 <rayenomri23p@gmail.com>
2025-04-18 13:11:05 +0100

    fixing charts time

--- frontend/src/components/LineChartComponent/LineChartComponent.jsx
+++ frontend/src/components/LineChartComponent/LineChartComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { LineChart } from '@mui/x-charts/LineChart';
 
 const LineChartComponent = ({ title, xData, seriesData }) => {
@@ -6,13 +6,29 @@ const LineChartComponent = ({ title, xData, seriesData }) => {
     <div className="chart-container">
       <h3>{title}</h3>
       <LineChart
-        xAxis={[{ data: xData }]}
+        xAxis={[
+          {
+            data: xData,
+            scaleType: 'time',
+            valueFormatter: (value) => {
+              const date = new Date(value);
+              // Format to DD/YYYY/MM HH:mm:ss in UTC
+              const day = String(date.getUTCDate()).padStart(2, '0');
+              const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+              const year = date.getUTCFullYear();
+              const hours = String(date.getUTCHours()).padStart(2, '0');
+              const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+              const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+              return `${day}/${month}  [${hours}:${minutes}:${seconds}]`;
+            },
+          },
+        ]}
         series={[{ data: seriesData }]}
         width={500}
         height={300}
       />
     </div>
-  )
-}
+  );
+};
 
-export default LineChartComponent
+export default LineChartComponent;
